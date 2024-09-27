3b703625995abbd2a5279b007cada1dfb43437e8
michaelreinprecht <reinprecht.michael.business@gmail.com>
2024-09-27 08:52:55 +0200

    Testing udp_server example....

--- TrailBlazorServerApp/TrailBlazorServerApp/obj/Debug/net8.0/TrailBlazorServerApp.AssemblyInfo.cs
+++ TrailBlazorServerApp/TrailBlazorServerApp/obj/Debug/net8.0/TrailBlazorServerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrailBlazorServerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a8c04ae68cabbc45f6c7ee033d42760f3c544f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrailBlazorServerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrailBlazorServerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
