a622212dcf8cecc5e84da9486b86199c4819dc88
michaelreinprecht <reinprecht.michael.business@gmail.com>
2024-10-03 08:08:21 +0200

    Further testing on sending structs

--- TrailBlazorServerApp/TrailBlazorServerApp/Data/Command.cs
+++ TrailBlazorServerApp/TrailBlazorServerApp/Data/Command.cs
@@ -5,7 +5,7 @@ namespace TrailBlazorServerApp.Data
     [StructLayout(LayoutKind.Sequential, Pack = 1)]  // Ensure 1-byte alignment like in C
     public struct Command
     {
-        public char Direction;  // Same as 'char' in C
+        public byte Direction;  // Same as 'char' in C
         public int Stop;        // Same as 'int' in C
         public bool StopBool;   // Same as 'bool' in C
     }
