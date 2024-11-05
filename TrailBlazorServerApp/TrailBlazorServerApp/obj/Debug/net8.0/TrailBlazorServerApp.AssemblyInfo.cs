c08bc7a351d5a1d70fef5695fcebb757c2263872
michaelreinprecht <reinprecht.michael.business@gmail.com>
2024-11-05 09:56:11 +0100

    Moved css back into style tag

--- TrailBlazorServerApp/TrailBlazorServerApp/obj/Debug/net8.0/TrailBlazorServerApp.AssemblyInfo.cs
+++ TrailBlazorServerApp/TrailBlazorServerApp/obj/Debug/net8.0/TrailBlazorServerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrailBlazorServerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d49341eac0875fb3f40ff5f7bd2fdcf68591cd17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a506eca12b90b479b1df65dc357a465787ee2ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrailBlazorServerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrailBlazorServerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
